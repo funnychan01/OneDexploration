60e8c2c7e0b4cde9efb282b2b53d881bdae589bf
Jackie <jackie.chan@oocl.com>
2018-09-18 17:01:29 +0800

    initialized project components

---
+++ env.py
@@ -0,0 +1,13 @@
+def update_env(S, episode, step_counter):
+    # This is how environment be updated
+    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
+    if S == 'terminal':
+        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(2)
+        print('\r                                ', end='')
+    else:
+        env_list[S] = 'o'
+        interaction = ''.join(env_list)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(FRESH_TIME)
---
+++ main.py
@@ -0,0 +1,9 @@
+from env import *
+from brain import *
+
+"""
+A simple example for Reinforcement Learning using table lookup Q-learning method.
+An agent "o" is on the left of a 1 dimensional world, the treasure is on the rightmost location.
+Run this program and to see how the agent will improve its strategy of finding the treasure.
+"""
+
