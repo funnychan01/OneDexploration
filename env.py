beaaec5bf0b9e17d437581ecd8da7df21c1ae697
Jackie <jackie.chan@oocl.com>
2018-09-24 14:59:01 +0800

    completed transforming codes to OOP style

--- env.py
+++ env.py
@@ -3,23 +3,19 @@ import time
 
 class Environment:
 
-    def __init__(self, S, episode, step_counter, n_states, fresh_time):
-        self.S = S
-        self.episode = episode
-        self.step_counter = step_counter
-        self.n_states = n_states
-        self.fresh_time = fresh_time
+    def __init__(self):
+        pass
 
-    def update_env(self):
-        # This is how environment be updated
-        env_list = ['-'] * (self.n_states - 1) + ['T']  # '---------T' our environment
-        if self.S == 'terminal':
-            interaction = 'Episode %s: total_steps = %s' % (self.episode + 1, self.step_counter)
+    def update_env(self, S, episode, step_counter, n_states, fresh_time):
+
+        env_list = ['-'] * (n_states - 1) + ['T']
+        if S == 'terminal':
+            interaction = 'Episode %s: total_steps = %s' % (episode + 1, step_counter)
             print('\r{}'.format(interaction), end='')
             time.sleep(2)
             print('\r                                ', end='')
         else:
-            env_list[self.S] = 'o'
+            env_list[S] = 'o'
             interaction = ''.join(env_list)
             print('\r{}'.format(interaction), end='')
-            time.sleep(self.fresh_time)
+            time.sleep(fresh_time)
--- main.py
+++ main.py
@@ -2,52 +2,55 @@ from env import Environment
 from brain import Brain
 
 """
-A simple example for Reinforcement Learning using table lookup Q-learning method.
-An agent "o" is on the left of a 1 dimensional world, the treasure is on the rightmost location.
-Run this program and to see how the agent will improve its strategy of finding the treasure.
-
 Q-learning application.
 """
 
 
-class Runner:
+class Runner(object):
 
     def __init__(self, env, brain):
         self.env = env
         self.brain = brain
+        self.n_states = 6
+        self.actions = ["left", "right"]
+        self.max_episodes = 10
+        self.fresh_time = 0.3
+        self.alpha = 0.1
+        self.gamma = 0.9
 
     def start(self):
-        q_table = self.brain.build_q_table()
 
-        for episode in range(self.brain.max_episode):
+        q_table = self.brain.build_q_table(self.n_states, self.actions)
+
+        for episode in range(self.max_episodes):
+
             step_counter = 0
             S = 0
             is_terminated = False
-            self.env.update_env()
+            self.env.update_env(S, episode, step_counter, self.n_states, self.fresh_time)
 
             while not is_terminated:
-                A = self.brain.choose_action(S, self.brain.q_table)
-                self.brain.S_, self.brain.R = self.brain.get_env_feedback(S, A)
+
+                A = self.brain.choose_action(S, q_table, self.actions)
+                S_, R = self.brain.get_env_feedback(S, A, self.n_states)
                 q_predict = q_table.loc[S, A]
-                if self.brain.S_ != "terminal":
-                    q_target = self.brain.R + self.brain.gamma * self.brain.q_table.iloc[self.brain.S_, :].max()
+                if S_ != 'terminal':
+                    q_target = R + self.gamma * q_table.iloc[S_, :].max()
                 else:
-                    q_target = self.brain.R
+                    q_target = R
                     is_terminated = True
 
-                q_table.loc[S, A] += self.brain.alpha * (q_target - q_predict)
-                S = self.brain.S_
+                q_table.loc[S, A] += self.alpha * (q_target - q_predict)
+                S = S_
 
-                self.env.update_env(S, self.evn.episode, step_counter + 1, self.brain.n_states)
-
+                self.env.update_env(S, episode, step_counter + 1, self.n_states, self.fresh_time)
                 step_counter += 1
 
-            print(q_table)
+        print('\r\nQ-table:\n')
+        print(q_table)
 
 
 if __name__ == "__main__":
-    Brain = Brain()
-    Env = Environment(0, 0, 0, 0)
-    runner = Runner(Env, Brain)
-    print("\r\nQ-table:\n")
+    runner = Runner(Environment(), Brain())
+    print("\r\n1-D Treasure Hunting Adventure Using Q-Learning:\n")
     runner.start()
